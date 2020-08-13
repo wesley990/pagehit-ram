c41447fdeafe8eac3d7a32e3fa5a41eb451bb8ab
wesley <shlchliu@cn.ibm.com>
2020-08-13 11:23:05 +0800

    fixed by eslint

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
 // main application
 
-'use strict';
+'use strict'
 
 const
   // default HTTP port
-  port = 3000,
+  port = 3000
 
-  // Node.js modules
-  http = require('http'),
-  url = require('url'),
+// Node.js modules
+const http = require('http')
+const url = require('url')
 
-  // page counter object
-  pagehit = new (require('./lib/pagehit'))(),
+// page counter object
+const pagehit = new (require('./lib/pagehit'))()
 
-  // HTTP response
-  httpResponse = require('./lib/httpresponse');
-
+// HTTP response
+const httpResponse = require('./lib/httpresponse')
 
 // new server
 http.createServer((req, res) => {
-
   // get referring page count
-  let count = pagehit.count(req);
+  const count = pagehit.count(req)
 
   // error: referring page not found
   if (!count) {
-
-    httpResponse({ res, status: 400, content: 'No referrer' });
-    return;
-
+    httpResponse({ res, status: 400, content: 'No referrer' })
+    return
   }
 
   // send appropriate response
-  let uri = url.parse(req.url).pathname;
+  const uri = url.parse(req.url).pathname
   switch (uri) {
-
     // Ajax counter
     case '/counter.json':
 
@@ -42,9 +37,9 @@ http.createServer((req, res) => {
         res,
         mime: 'application/json',
         content: `{"counter":${count}}`
-      });
+      })
 
-      break;
+      break
 
     // JavaScript document.write() counter
     case '/counter.js':
@@ -53,8 +48,8 @@ http.createServer((req, res) => {
         res,
         mime: 'application/javascript',
         content: `document.write('<span class="pagecounter">${count}</span>');`
-      });
-      break;
+      })
+      break
 
     // JavaScript deferred DOM counter
     case '/counter-defer.js':
@@ -63,8 +58,8 @@ http.createServer((req, res) => {
         res,
         mime: 'application/javascript',
         content: `window.addEventListener('DOMContentLoaded',()=>{let c=document.querySelectorAll('script[src$="/counter-defer.js"]');for(let i=0;i<c.length;i++) c[i].insertAdjacentHTML('afterend','<span class="pagecounter">${count}</span>');});`
-      });
-      break;
+      })
+      break
 
     // SVG counter
     case '/counter.svg':
@@ -73,16 +68,14 @@ http.createServer((req, res) => {
         res,
         mime: 'image/svg+xml',
         content: `<svg xmlns="http://www.w3.org/2000/svg" width="${String(count).length * 0.6}em" height="1em"><style>text { font-family: sans-serif; font-size: 1em; dominant-baseline: middle; }</style><text y="50%">${count}</text></svg>`
-      });
-      break;
+      })
+      break
 
     // error: invalid HTTP request
     default:
-      httpResponse({ res, status: 404, content: 'Not found' });
-      break;
-
+      httpResponse({ res, status: 404, content: 'Not found' })
+      break
   }
+}).listen(port)
 
-}).listen(port);
-
-console.log(`page hit web service running on port ${port}`);
+console.log(`page hit web service running on port ${port}`)
